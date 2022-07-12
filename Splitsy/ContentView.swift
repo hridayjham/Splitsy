1d3c605938b3c0037f29e380ef2e47d53ae2b7c3
hridayjham <jhamh@mcmaster.ca>
2022-07-12 17:37:01 -0400

    Implemented Sign up function

--- Splitsy/ContentView.swift
+++ Splitsy/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct ContentView: View {
     @State private var isAuthenticated = false
@@ -14,51 +16,103 @@ struct ContentView: View {
     @State private var password = ""
     
     var body: some View {
-        
-        if hasAccount {
-            
-            NavigationView {
-                VStack {
-                    TextField("email", text: $email)
+        if isAuthenticated == false {
+            if hasAccount {
+                
+                NavigationView {
+                    VStack {
+                        TextField("email", text: $email)
+                            .padding()
+                        TextField("password", text: $password)
+                            .padding()
+                        Button("Log in") {
+                            
+                        }
                         .padding()
-                    TextField("password", text: $password)
+                        Button("Sign up") {
+                            hasAccount.toggle()
+                        }
                         .padding()
-                    Button("Log in") {
-                        
                     }
-                    .padding()
-                    Button("Already have an account? Log in") {
-                        hasAccount.toggle()
-                    }
-                    .padding()
                 }
                 .navigationTitle("Log in")
                 .font(.headline)
+                
+            } else {
+                
+                NavigationView {
+                    VStack {
+                        TextField("email", text: $email)
+                            .padding()
+                        TextField("password", text: $password)
+                            .padding()
+                        Button("Sign Up") {
+                                SignUp()
+                        }
+                        .padding()
+                        Button("Already have an account? Log in") {
+                            hasAccount.toggle()
+                        }
+                        .padding()
+                    }
+                }
+                .navigationTitle("Sign up")
+                .font(.headline)
+                
             }
-            
         } else {
-            
-            NavigationView {
-                VStack {
-                    TextField("email", text: $email)
-                        .padding()
-                    TextField("password", text: $password)
-                        .padding()
-                    Button("Log in") {
-                        
-                    }
-                    .padding()
-                    Button("Already have an account? Log in") {
-                        hasAccount.toggle()
-                    }
-                    .padding()
-                }
-                .navigationTitle("Log in")
-                .font(.headline)
-            }
-            
+            SuccessfulSignInView()
         }
         
+        
+    }
+}
+
+extension ContentView {
+    
+    func SignUp() {
+        if email.isEmpty {
+            print("No email provided")
+            return
+        } else if password.isEmpty {
+            print("No password provided")
+            return
+        } else {
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                guard let user = authResult?.user, error == nil else {
+                    print("error: \(error?.localizedDescription)")
+                    return
+                }
+            }
+            isAuthenticated = true
+        }
+    }
+    
+    func validateFields() {
+        if email.isEmpty {
+            print("No email provided")
+            return
+        } else if password.isEmpty {
+            print("No password entered")
+            return
+        } else {
+         //   login(email: email, pass: password)
+        }
+    }
+    
+    /*func login(email: String, pass: String, completionBlock: @escaping (_ success: Bool) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: pass) { (result, error) in
+            if let error = error, let _ = AuthErrorCode(rawValue: error._code) {
+                completionBlock(false)
+            } else {
+                completionBlock(true)
+            }
+        }
+        isAuthenticated = true
+    }*/
+    
+    func checkUserInfo() {
+            
     }
 }
 
---
+++ Splitsy/SuccessfulSignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SuccessfulSignInView.swift
+//  Splitsy
+//
+//  Created by Hriday Jham on 2022-07-12.
+//
+
+import SwiftUI
+
+struct SuccessfulSignInView: View {
+    var body: some View {
+        Text("Successfully signed in")
+    }
+}
+
+struct SuccessfulSignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SuccessfulSignInView()
+    }
+}
